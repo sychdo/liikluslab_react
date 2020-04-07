db92cd7c946049081c0c1672ea009551f37a504b
Dmytro Sych <d.sych@sana-commerce.com>
2020-04-07 14:20:24 +0300

    Updates

---
+++ precache-manifest.0d2add56ef4e41edc83480d771751062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b5460d938f553ff1d2dfb02b44e263",
+    "url": "/liikluslab_react/index.html"
+  },
+  {
+    "revision": "3285acb6ccc7bf638bfb",
+    "url": "/liikluslab_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3ec660d923bf0df6ee1",
+    "url": "/liikluslab_react/static/js/2.3d97df43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liikluslab_react/static/js/2.3d97df43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3285acb6ccc7bf638bfb",
+    "url": "/liikluslab_react/static/js/main.7ee9c7b7.chunk.js"
+  },
+  {
+    "revision": "2fbe5c992cbf33405220",
+    "url": "/liikluslab_react/static/js/runtime-main.7e0a8dfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/liikluslab_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
