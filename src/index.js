d9737fddd424b297c8edb118fc5eae6567dbf785
Dmytro Sych <d.sych@sana-commerce.com>
2020-04-08 14:50:32 +0300

    fix app entry point

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
-class MyType {}
-
-class ObjectManager {
-  static #privateField = 2;
-  static x = 2;
-  static getInstance(Type) {
-    console.log(ObjectManager.#privateField);
-    return new Type();
-  }
-}
-
-const instance = ObjectManager.getInstance(MyType);
-console.log(instance);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
