80f18d1c4996efdd46787aee5226ef2705066f44
Dmytro Sych <d.sych@sana-commerce.com>
2020-04-08 14:46:39 +0300

    add localization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.querySelector('meta[name="description"]').attributes['content'].value = "abc content";
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+            <section className="banner">
+                <div className="banner-content content">
+                    <img alt="banner" src="assets/images/banner.png" />
+                </div>
+            </section>
+            <div className="main-content content">
+                <h1>Liikluslab поможет сдать экзамены с первой попытки</h1>
+                <h2>Онлайн учебники от создателей новых экзаменационных вопросов </h2>
+            </div>
+        </main>
   );
 }
 
